934d158cb6c3aac63db74aead7b7072a422c21a4
Russell Currey <ruscur@russell.cc>
2021-07-30 18:28:08 +1000

    Fix the ppc bn_mul_mont() to use the new prefix system
    The symbol prefix changes that began in commit 384f7d056b1 require
    that cpu-ppc.c finds the generated prefix header and that bigint.rs
    looks for the correctly prefixed bn_mul_mont().
    
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/arithmetic/bigint.rs
+++ src/arithmetic/bigint.rs
@@ -1347,7 +1347,8 @@ fn limbs_mont_square(r: &mut [Limb], m: &[Limb], n0: &N0) {
     target_arch = "aarch64",
     target_arch = "arm",
     target_arch = "x86_64",
-    target_arch = "x86"
+    target_arch = "x86",
+    target_arch = "powerpc64"
 ))]
 prefixed_extern! {
     // `r` and/or 'a' and/or 'b' may alias.
